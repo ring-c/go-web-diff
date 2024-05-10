6c5cd90bf35fd0f8dc6db69e7fd3d72e99909cc7
Eugene Denisenko <caravus@craft.sc>
2024-05-10 14:53:46 +0500

    Work
    Former-commit-id: 8eac0ba57fed5a55fb3a053405913c4fa7e7e07d

--- pkg/txt2img/init.go
+++ pkg/txt2img/init.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 
+	"github.com/davecgh/go-spew/spew"
 	"gorgonia.org/tensor"
 )
 
 func main() {
-	print("\n\n\n\n\n\n\n")
+	spew.Config.Indent = "\t"
 
-	a := tensor.New(tensor.WithShape(2, 2), tensor.WithBacking([]int{1, 2, 3, 4}))
+	a := tensor.New(tensor.WithBacking(tensor.Range(tensor.Float32, 0, 192)), tensor.WithShape(8, 8, 3))
+
 	fmt.Printf("a:\n%v\n", a)
 
+	data, err := tensorToImage(a)
+	if err != nil {
+		panic(err.Error())
+		return
+	}
+
+	spew.Dump(data)
 }
