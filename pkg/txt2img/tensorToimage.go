29ae713d54a86958d6931567e8cfa197df90fec1
Eugene Denisenko <caravus@craft.sc>
2024-05-10 15:52:00 +0500

    Work
    Former-commit-id: d3503872cbf2d4cff12437f1884cf690dabf4bfa

--- pkg/txt2img/tensorToimage.go
+++ pkg/txt2img/tensorToimage.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"errors"
+	"image"
+	"image/color"
 
 	"gorgonia.org/tensor"
 )
 
-func tensorToImage(input *tensor.Dense) (data []byte, err error) {
+func tensorToImage(input *tensor.Dense) (img *image.RGBA, err error) {
 	var shape = input.Shape()
 
 	width, err := shape.DimSize(0)
@@ -22,41 +24,64 @@ func tensorToImage(input *tensor.Dense) (data []byte, err error) {
 		return
 	}
 
-	if channels != 3 { // || input.typ != GGMLTypeF32
+	if channels != 3 || input.Dtype() != tensor.Float32 {
 		err = errors.New("invalid input tensor")
 		return
 	}
 
-	data = make([]byte, width*height*channels)
+	img = image.NewRGBA(image.Rect(0, 0, width, height))
 
-	for iy := 0; iy < int(height); iy++ {
-		for ix := 0; ix < int(width); ix++ {
-			for k := 0; k < int(channels); k++ {
-				var value float32
-				value, err = tensorGetF32(input, ix, iy, k)
-				if err != nil {
-					return
-				}
+	for iy := 0; iy < height; iy++ {
+		for ix := 0; ix < width; ix++ {
+			// for k := 0; k < channels; k++ {
+			//
+			//
+			// 	data[iy*width*channels+ix*channels+k] = uint8(value * 255.0)
+			// }
 
-				data[iy*int(width)*int(channels)+ix*int(channels)+k] = uint8(value * 255.0)
+			var R, G, B uint8
+
+			R, err = tensorGetF32(input, ix, iy, 0)
+			if err != nil {
+				return
 			}
+
+			G, err = tensorGetF32(input, ix, iy, 1)
+			if err != nil {
+				return
+			}
+
+			B, err = tensorGetF32(input, ix, iy, 2)
+			if err != nil {
+				return
+			}
+
+			img.Set(ix, iy, color.RGBA{
+				R: R,
+				G: G,
+				B: B,
+				A: 255,
+			})
+
 		}
 	}
 
 	return
 }
 
-func tensorGetF32(tx *tensor.Dense, x, y, z int) (data float32, err error) {
+func tensorGetF32(tx *tensor.Dense, x, y, z int) (data uint8, err error) {
 	value, err := tx.At(x, y, z)
 	if err != nil {
 		return
 	}
 
-	data, ok := value.(float32)
+	val, ok := value.(float32)
 	if !ok {
 		err = errors.New("tensor value not float32")
 		return
 	}
+
+	data = uint8(val * 255.0)
 
 	return
 }
