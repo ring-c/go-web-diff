6a93e0de6d9f979f7f7afec7f31c6232da60c223
Eugene Denisenko <caravus@craft.sc>
2024-06-23 14:00:41 +0500

    Work
    Former-commit-id: 72240791fc9f027f3879c68f51718051544c5a37

--- pkg/txt2img/generate.go
+++ pkg/txt2img/generate.go
@@ -2,9 +2,16 @@ package txt2img
 
 import (
 	"errors"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"io"
 	"math/rand"
-
-	"github.com/davecgh/go-spew/spew"
+	"os"
+	"path"
+	"time"
+	"unsafe"
 )
 
 type SDContext struct {
@@ -49,10 +56,10 @@ const (
 
 // var encoder = make(map[string]int)
 
-func (gen *Generator) Generate(prompt string, width, height int) (result *SDImage, err error) {
+func (gen *Generator) Generate(prompt string, width, height int) (err error) {
 
 	// negativePrompt := ""
-	clipSkip := 2
+	// clipSkip := 2
 	// cfgScale := 1.0
 	var sampleStepsInput = 16
 	seed := uint64(42)
@@ -68,14 +75,18 @@ func (gen *Generator) Generate(prompt string, width, height int) (result *SDImag
 
 	var schedule = KarrasSchedule{}
 
-	sigmas := schedule.GetSigmas(sampleStepsInput)
+	var sigmas = schedule.GetSigmas(sampleStepsInput)
+	// spew.Dump(sigmas)
 
 	// Get learned condition
 	// c, cVector := getLearnedCondition(workCtx, prompt, clipSkip, width, height, false)
 
-	var pair = gen.GetLearnedCondition(gen.Model.GetCTX(), workCtx, prompt, width, height, clipSkip)
-	var c = gen.PairGet(pair, true)
-	var cVector = gen.PairGet(pair, false)
+	// var pairCond = gen.GetLearnedCondition(gen.Model.GetCTX(), workCtx, prompt, width, height, clipSkip)
+
+	// spew.Dump(pairCond)
+
+	// var c = gen.PairGet(pair, true)
+	// var cVector = gen.PairGet(pair, false)
 
 	// spew.Dump(c)
 	// spew.Dump(cVector)
@@ -95,38 +106,30 @@ func (gen *Generator) Generate(prompt string, width, height int) (result *SDImag
 
 	// x0 := sdCtx.SD.SampleGo(workCtx, xT, c, cVector, sigmas)
 
-	var x0 = gen.GoSample(gen.Model.GetCTX(), workCtx, xT, c, cVector, len(sigmas), sigmas)
+	var cImageData = gen.GoSample(gen.Model.GetCTX(), workCtx, xT, len(sigmas), sigmas)
 
 	// BATCH END
 
-	spew.Dump(x0)
+	var imgData = unsafe.Slice((*byte)(cImageData), 3*width*height)
+	var data = bytesToImage(imgData, width, height)
 
-	/*
+	var filename = fmt.Sprintf("%d-%d.png", time.Now().Unix(), seed)
+	var file *os.File
+	file, err = os.Create(path.Join(".", filename))
+	if err != nil {
+		return
+	}
 
-		if sdCtx.SD.FreeParamsImmediately {
-			sdCtx.SD.DiffusionModel.FreeParamsBuffer()
-		}
+	err = imageToWriter(data, file)
+	if err != nil {
+		return
+	}
 
-		// Decode to image
-		decodedImages := make([]*GGMLTensor, 0)
-		img := sdCtx.SD.DecodeFirstStage(workCtx, x0)
-		if img != nil {
-			decodedImages = append(decodedImages, img)
-		}
+	err = file.Close()
+	if err != nil {
+		return
+	}
 
-		log.Info("decode_first_stage completed")
-		if sdCtx.SD.FreeParamsImmediately && !sdCtx.SD.UseTinyAutoencoder {
-			sdCtx.SD.FirstStageModel.FreeParamsBuffer()
-		}
-
-		result := &SDImage{
-			Width:   width,
-			Height:  height,
-			Channel: 3,
-			Data:    SDTensorToImage(decodedImages[0]),
-		}
-		// GGMLFree(workCtx)
-	*/
 	return
 }
 
@@ -164,4 +167,35 @@ type GGMLType int
 type ConditionPair struct {
 	First  *GGMLTensor
 	Second *GGMLTensor
+}
+
+func bytesToImage(byteData []byte, width, height int) (img *image.RGBA) {
+	img = image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			idx := (y*width + x) * 3
+			img.Set(x, y, color.RGBA{
+				R: byteData[idx],
+				G: byteData[idx+1],
+				B: byteData[idx+2],
+				A: 255,
+			})
+		}
+	}
+
+	return
+}
+
+func imageToWriter(image *image.RGBA, writer io.Writer) (err error) {
+	var enc = png.Encoder{
+		CompressionLevel: png.BestSpeed,
+	}
+
+	err = enc.Encode(writer, image)
+	if err != nil {
+		return
+	}
+
+	return
 }
